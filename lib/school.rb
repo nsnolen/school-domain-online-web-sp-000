0769a10f34f4283adf553ec7dcb972641fefb68b
Sara Nolen <nolensara1@gmail.com>
2021-01-17 17:44:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -19,7 +19,9 @@ end
 
      def sorted
        sorted_roster = {}
-       @roster.sort.each{|grade, name| sorted_roster[grade] = (name.sort)}
+       @roster.sort.each do |grade, name| 
+         sorted_roster[grade] = (name.sort)
+       end
        sorted_roster
      end
    end
