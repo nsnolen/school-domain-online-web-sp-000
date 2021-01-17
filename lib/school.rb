3c5d0f0ac59277d42461abe340145d24431d0fd9
Sara Nolen <nolensara1@gmail.com>
2021-01-17 17:32:47 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,6 +6,9 @@ class School
       @roster = {}
     end
 
+    def grade(grade)
+      @roster[grade] = grade
+      
     def add_student(students_name, grade)
       @roster << grade.to_i[students_name]
     end
