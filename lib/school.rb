351829fcfcad8d6d3802523d7c0bb60db4d5a67c
Sara Nolen <nolensara1@gmail.com>
2021-01-17 17:25:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,8 @@
 class School
-  attr_accessor = :roster
-  def initialize(name, roster)
+  attr_accessor = :name, :roster
+  def initialize(name)
     @name = name
-    @roster = []
+    @roster = {}
   end
 
   def name
